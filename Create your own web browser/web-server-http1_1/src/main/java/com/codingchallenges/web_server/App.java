d42013d92ad692cf702c56126cfeaa5529e55429
Shashwenth <64272462+Shashwenth@users.noreply.github.com>
2024-12-19 23:05:48 -0500

    Implemented Multi Threading
    Implemented Func:
    1. Implemented Wifi Access
    2. Implemented Multi Threading

--- Create your own web browser/web-server-http1_1/src/main/java/com/codingchallenges/web_server/App.java
+++ Create your own web browser/web-server-http1_1/src/main/java/com/codingchallenges/web_server/App.java
@@ -1,17 +1,11 @@
 package com.codingchallenges.web_server;
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketTimeoutException;
 
 import com.codingchallenges.web_server.ConfigurationMapper.JsonParser;
 import com.codingchallenges.web_server.ConfigurationModel.JsonEntity;
+import com.codingchallenges.web_server.ThreadImplementation.ThreadStartUp;
 
 /**
  * Hello world!
@@ -32,46 +26,14 @@ public final class App {
         jsonParser.printJsonEntity();
         JsonEntity jsonEntity = jsonParser.getJsonEntity();
 
-        try (ServerSocket serverSocket = new ServerSocket(jsonEntity.getPORT(), 50, InetAddress.getByName("10.0.0.25"));) {
-                try (Socket socket = serverSocket.accept()) {
-                   // socket.setSoTimeout(3000);
-                    
-                    try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    OutputStream outputStream = socket.getOutputStream();) {
-                        String inputLine;
-                        
-                        while ((inputLine = in.readLine()) != null && !inputLine.isEmpty()) {
-                            System.out.println("Received: " + inputLine);
-                        }
-                        
-                        System.out.println("Request complete, sending response...");
-
-                        
-                        String html = "<html><head><title>My Web Server</title></head><body><h1>Welcome to my web server!</h1></body></html>";
-                        final String CRLF = "\r\n";
-                        String response = "HTTP/1.1 200 OK" + CRLF +
-                                "Content-Type: text/html" + CRLF +
-                                "Content-Length: " + html.getBytes().length + CRLF +
-                                CRLF +
-                                html;
-                        
-                        outputStream.write(response.getBytes());
-                        outputStream.flush();
-                        socket.close();
-                        serverSocket.close();
-                        outputStream.close();
-                    }
-
-                } catch (SocketTimeoutException e) {
-                    System.err.println("Connection timed out: " + e.getMessage());
-                } catch (IOException e) {
-                    System.err.println("Connection error: " + e.getMessage());
-                }
-            } catch (IOException e) {
-                System.err.println("Server error: " + e.getMessage());
-            }
-
-
+        ThreadStartUp threadStartUp;
+        try {
+            threadStartUp = new ThreadStartUp(jsonEntity.getPORT(), jsonEntity.getWebRoot());
+            threadStartUp.start();
+        } catch (IOException ex) {
+            System.err.println("Error: " + ex.getMessage());
+        } 
+        
     }
 
 
---
+++ Create your own web browser/web-server-http1_1/src/main/java/com/codingchallenges/web_server/ThreadImplementation/ThreadStartUp.java
@@ -0,0 +1,52 @@
+package com.codingchallenges.web_server.ThreadImplementation;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ThreadStartUp extends Thread {
+
+    private final int port;
+    @SuppressWarnings("unused")
+    private final  String webRoot;
+
+    private final ServerSocket serverSocket;
+    
+    public ThreadStartUp(int port, String webRoot) throws IOException{
+        this.port = port;
+        this.webRoot = webRoot;
+        this.serverSocket = new ServerSocket(this.port, 50, InetAddress.getByName("10.0.0.25"));
+    }
+
+
+    @Override
+    public void run(){
+        
+        try {
+            while(serverSocket.isBound() && !serverSocket.isClosed()){
+                try {
+                    Socket socket = serverSocket.accept();
+                    
+                    ThreadSocketImplementation threadSocketImplementation = new ThreadSocketImplementation(socket);
+                    threadSocketImplementation.start();
+                    
+                } catch (IOException ex) {
+                }
+                
+        }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        } finally{
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+        
+        
+    }
+
+    
+
+}
